8d69b5153bb50c6296e9891e53b59e70479fd713
linuxbuh <linuxbuh@yandex.ru>
2022-08-17 12:32:18 +0300

    Update install-zabbix-agent-centos7.sh

--- linux/install-zabbix-agent-centos7.sh
+++ linux/install-zabbix-agent-centos7.sh
@@ -2,7 +2,7 @@
 
 cd /root
 
-yum install  https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
+yum install https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
 
 yum install zabbix-agent
 
