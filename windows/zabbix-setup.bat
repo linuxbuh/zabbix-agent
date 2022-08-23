99c2a391329d34b86ee65eeb0de25bc5d0e169b4
linuxbuh <linuxbuh@yandex.ru>
2022-08-23 12:00:34 +0300

    update

--- windows/zabbix-setup.bat
+++ windows/zabbix-setup.bat
@@ -1,4 +1,15 @@
-wget -O C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi https://cdn.zabbix.com/zabbix/binaries/stable/5.0/5.0.26/zabbix_agent-5.0.26-windows-amd64-openssl.msi
-msiexec /i C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi /qn /norestart SERVER=77.232.21.123,62.112.124.182,91.206.14.21,91.206.14.41,188.246.234.24/29 LISTENPORT=10050 ENABLEPATH=1
-rm C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi
-rm C:\zabbix-setup.bat
+rem C:\wget.exe -O C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi https://cdn.zabbix.com/zabbix/binaries/stable/5.0/5.0.26/zabbix_agent-5.0.26-windows-amd64-openssl.msi
+rem msiexec /i C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi /qn /norestart SERVER=77.232.21.123,62.112.124.182,91.206.14.21,91.206.14.41,188.246.234.24/29 LISTENPORT=10050 ENABLEPATH=1
+rem rm C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi
+rem rm C:\zabbix-setup.bat
+@echo off
+ 
+:: COMPANY NAME:
+set /p COMPANY="COMPANY NAME:: "
+ 
+:: COMPANY:
+echo "YUO ENTER %COMPANY%"
+
+C:\wget.exe -O 
+
+pause 30
