e93b594b72dd0c250b37d8b5fdc221329dff0171
linuxbuh <linuxbuh@yandex.ru>
2022-08-25 10:20:30 +0300

    Update zabbix-setup.bat

--- windows/zabbix-setup.bat
+++ windows/zabbix-setup.bat
@@ -13,11 +13,13 @@ set /p PROXY="ENTER ZABBIX PROXY IP:: "
 echo "YUO ENTER ZABBIX PROXY %PROXY%"
 
 C:\wget.exe -O C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi https://cdn.zabbix.com/zabbix/binaries/stable/5.0/5.0.26/zabbix_agent-5.0.26-windows-amd64-openssl.msi
-pause
+pause 10
 msiexec /i C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi /qn /norestart SERVER=%PROXY%,77.232.21.123,62.112.124.182,91.206.14.21,91.206.14.41,188.246.234.24/29 LISTENPORT=10050 ENABLEPATH=1
 del C:\zabbix_agent-5.0.26-windows-amd64-openssl.msi
-pause
+pause 10
 
 net stop "Zabbix Agent"
+pause 10
 net start "Zabbix Agent"
+
 exit /b
