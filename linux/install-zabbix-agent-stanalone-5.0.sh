2e8ff8228eb6be97fb1d9d3c96933e852f0759c8
linuxbuh <linuxbuh@yandex.ru>
2022-08-31 12:34:28 +0300

    Update install-zabbix-agent-stanalone-5.0.sh

--- linux/install-zabbix-agent-stanalone-5.0.sh
+++ linux/install-zabbix-agent-stanalone-5.0.sh
@@ -43,103 +43,110 @@ echo -e "\e[1;33;4;44mВаш дистрибутив LINUX - $OSRELEASE\e[0m"
 
 #Устанавливаем какие пакеты качать для Debian и Ubuntu
 	if [ $PAKETMANAGER = DEB ]; then
-
-	
+		  			
 		if [ $VERSION_OSRELEASE = 11 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bdebian11_all.deb https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bdebian11_all.deb
+			wget -O /tmp/zabbix-release_5.0-2%2Bdebian11_all.deb https://repo.zabbix.com/zabbix/5.0/debian/pool/main/z/zabbix-release/zabbix-release_5.0-2%2Bdebian11_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bdebian11_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-2%2Bdebian11_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 10 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bdebian10_all.deb https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bdebian10_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Bbuster_all.deb https://repo.zabbix.com/zabbix/5.0/debian/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Bbuster_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bdebian10_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Bbuster_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 9 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bdebian9_all.deb https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bdebian9_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Bstretch_all.deb https://repo.zabbix.com/zabbix/5.0/debian/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Bstretch_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bdebian9_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Bstretch_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 22.04 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bubuntu22.04_all.deb https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bubuntu22.04_all.deb
+			wget -O /tmp/zabbix-release_5.0-2%2Bubuntu22.04_all.deb https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-2%2Bubuntu22.04_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bubuntu22.04_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-2%2Bubuntu22.04_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 20.04 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bubuntu20.04_all.deb https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bubuntu20.04_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Bfocal_all.deb https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Bfocal_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bubuntu20.04_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Bfocal_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 18.04 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bubuntu18.04_all.deb https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bubuntu18.04_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Bbionic_all.deb https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Bbionic_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bubuntu18.04_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Bbionic_all.deb
 			
 		fi
 		
 		if [ $VERSION_OSRELEASE = 16.04 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bubuntu16.04_all.deb https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bubuntu16.04_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Bxenial_all.deb https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Bxenial_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bubuntu16.04_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Bxenial_all.deb
 			
 		fi
 		
-		if [ $VERSION_OSRELEASE = 16.04 ]; then
+		if [ $VERSION_OSRELEASE = 14.04 ]; then
 			
-			wget -O /tmp/zabbix-release_6.0-3%2Bubuntu14.04_all.deb https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3%2Bubuntu14.04_all.deb
+			wget -O /tmp/zabbix-release_5.0-1%2Btrusty_all.deb https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1%2Btrusty_all.deb
 
-			dpkg -i /tmp/zabbix-release_6.0-3%2Bubuntu14.04_all.deb
+			dpkg -i /tmp/zabbix-release_5.0-1%2Btrusty_all.deb
 			
 		fi
 
 			apt-get update
-			apt-get install zabbix-agent git
+			
+			apt-get install -y zabbix-agent git
+	
 			
 	fi
 
 #Устанавливаем какие пакеты качать для Rhel? Centos и производных
 	if [ $PAKETMANAGER = RPM ]; then
-		 
-		yum install -y deltarpm
+		  
+			mkdir /var/lib/zabbix
+			
+			touch /var/lib/zabbix/zabbix_proxy_db
+            
+			chown -R zabbix:zabbix /var/lib/zabbix
+			
+			yum install -y deltarpm
 			
 		if [ $VERSION_OSRELEASE = 9 ]; then
 			
-			rpm -ivh https://repo.zabbix.com/zabbix/6.0/rhel/9/x86_64/zabbix-release-6.0-3.el9.noarch.rpm
+			rpm -ivh https://repo.zabbix.com/zabbix/5.0/rhel/9/x86_64/zabbix-release-5.0-3.el9.noarch.rpm
 					
 		fi
 		
 		if [ $VERSION_OSRELEASE = 8 ]; then
 			
-			rpm -ivh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-2.el8.noarch.rpm
+			rpm -ivh https://repo.zabbix.com/zabbix/5.0/rhel/8/x86_64/zabbix-release-5.0-1.el8.noarch.rpm
 					
 		fi
 		
 		if [ $VERSION_OSRELEASE = 7 ]; then
 			
-			rpm -ivh https://repo.zabbix.com/zabbix/6.0/rhel/7/x86_64/zabbix-release-6.0-2.el7.noarch.rpm
+			rpm -ivh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
 					
 		fi
 		
 		if [ $VERSION_OSRELEASE = 6 ]; then
 			
-			rpm -ivh https://repo.zabbix.com/zabbix/6.0/rhel/6/x86_64/zabbix-release-6.0-2.el6.noarch.rpm
+			rpm -ivh https://repo.zabbix.com/zabbix/5.0/rhel/6/x86_64/zabbix-release-5.0-1.el6.noarch.rpm
 					
 		fi
 		
@@ -148,7 +155,9 @@ echo -e "\e[1;33;4;44mВаш дистрибутив LINUX - $OSRELEASE\e[0m"
 			
 			yum install -y zabbix-agent git
 			
+			
 	fi
+
 
 
 			git clone https://github.com/linuxbuh/zabbix-agent.git
